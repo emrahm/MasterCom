8774b64d857227b618c12339c939d53dac65a031
Emrah Mersinli <emrah.mesinli@cardtek.com>
2018-07-25 17:06:45 +0300

    new models

---
+++ MyApi/Clearing.Msc.Business.MasterCom/ModelData/CaseDetailStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clearing.Msc.Business.MasterCom.ModelData
+{
+    /// <summary>
+    /// Root object of case id
+    /// </summary>
+    public class CaseDetailStatus
+    {
+        /// <summary>
+        /// Case id list
+        /// </summary>
+        public List<CaseIdRequestResponse> caseFilingList { get; set; }
+    }
+}
